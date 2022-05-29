c98a26581b1fd07917d3fdc586ce24b94fad8966
sumansourabh9155 <sumansourabh9155@gmail.com>
2022-05-29 23:47:03 +0530

    Added click handler

--- src/component/ExpencesList/Expense.jsx
+++ src/component/ExpencesList/Expense.jsx
@@ -1,11 +1,20 @@
 import "./Expense.css";
 
 import ExpenseCard from "../Background/ExpenseCard";
+import React from "react";
+import { useState } from "react";
 
 const Expense = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const date = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
+
+  const clickhandel = () => {
+    setTitle("Updated!");
+    return console.log("clicked");
+  };
   return (
     <ExpenseCard className="expense-item">
       <div>
@@ -14,8 +23,9 @@ const Expense = (props) => {
         <div>{year}</div>
       </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickhandel}></button>
       </div>
     </ExpenseCard>
   );
